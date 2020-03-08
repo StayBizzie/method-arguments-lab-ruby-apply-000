3588ea3440aae5d44bef25fe3a89d7cdf95658ac
Christopher Boykin <biznessmain@gmail.com>
2020-03-08 18:12:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def #introdution (name)
+def introdution (name)
   puts "Hi, myname is #{name}."
 end
 
-def #introdution_with_language (name,language)
+def introdution_with_language (name,language)
 puts "Hi, my name is #{name} and i am learnign to program in #{language}"
 end
